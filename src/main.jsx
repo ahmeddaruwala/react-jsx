5d58585d12c500a0add5054bb738727492fae912
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2024-10-25 11:52:59 +0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of scientists!
+      </p>
+    </>
+  </StrictMode>
+);
